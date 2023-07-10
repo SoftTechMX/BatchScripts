955cfb06db0e5b317d2a5185f51f88b7edd418e6
BayronVazquez <bayronnjonathan@gmail.com>
2023-07-10 12:04:33 -0600

    Script para eliminar todos los archivos de un directorio

---
+++ deleteAllFiles.bat
@@ -0,0 +1,10 @@
+:: Descripcion: Este  programa  elimina  todos  los  archivos del directorio actual, el  
+:: programa  usa  Prompts  y  warnings.  Para  mas  informacion visita la documentacion
+:: oficial de microsoft.
+::
+:: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/del
+::
+:: ====================================================================================
+::    NOTA: TEN CUIDADO UNA VEZ ELIMINADOS LOS ARCHIVO NO HAY MANERA DE RECUPERARLOS.
+:: ====================================================================================
+DEL . dr
